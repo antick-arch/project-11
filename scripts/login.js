726eb5bce456f01e05df3d52479d2ed5f08ac6f1
antick-arch <e241003@grad.iiuc.ac.bd>
2026-03-09 19:22:12 +0600

    added login functionalities

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+console.log("Login js connected");
+
+document.getElementById("sign-btn").addEventListener('click',function(){
+    const user = document.getElementById('user-inp');
+    const username = user.value;
+    const pass = document.getElementById('user-pass');
+    const password = pass.value;
+
+    if(username == 'admin' && password == 'admin123'){
+        alert("login successful");
+        window.location.assign('/index.html');
+    }
+    else{
+        alert('try again');
+        return;
+    }
+})
