c9357106fcc90c57d4a3060e3f303c99c3309013
antick-arch <e241003@grad.iiuc.ac.bd>
2026-03-10 21:00:54 +0600

    added modal

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,11 @@ let allData = [];
 
 const allIssues = () => {
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
-    .then((res) => res.json())
-    .then((data) => {
-        allData = data.data;
-        displayIssues(allData);
-    });
+        .then((res) => res.json())
+        .then((data) => {
+            allData = data.data;
+            displayIssues(allData);
+        });
 }
 
 const displayIssues = (issues) => {
@@ -21,8 +21,7 @@ const displayIssues = (issues) => {
     issues.forEach(element => {
         const card = document.createElement('div');
         const date = new Date(element.createdAt);
-        const formattedDate = date.toLocaleDateString(); 
-
+        const formattedDate = date.toLocaleDateString();
         card.innerHTML = `
         <div class="p-4 space-y-3">
             <div class="flex justify-between">
@@ -55,6 +54,7 @@ const displayIssues = (issues) => {
             priorityContainer.classList.add('bg-[#EEEFF2]', 'py-[6px]', 'px-[25px]', 'rounded-full');
             priorityText.classList.add('text-[#9CA3AF]');
         }
+
         if (element.status === 'open') {
             statusContainer.innerHTML = `<img src="./assets/Open-Status.png" alt="">`;
             card.classList.add('card', 'bg-white', 'border-t-4', 'border-t-[#00A96E]', 'shadow');
@@ -62,6 +62,7 @@ const displayIssues = (issues) => {
             statusContainer.innerHTML = `<img src="./assets/Closed- Status .png" alt="">`;
             card.classList.add('card', 'bg-white', 'border-t-4', 'border-t-[#A855F7]', 'shadow');
         }
+
         element.labels.forEach(label => {
             const labelDiv = document.createElement('div');
             labelDiv.classList.add('py-[6px]', 'px-[8px]', 'rounded-full', 'font-medium', 'text-[12px]');
@@ -79,6 +80,10 @@ const displayIssues = (issues) => {
         });
 
         cardContainer.appendChild(card);
+        card.addEventListener('click', () => {
+            loadWordDetail(element.id);
+        })
+
     });
     manageSpinner(false);
 }
@@ -111,11 +116,11 @@ document.getElementById('btn-closed').addEventListener('click', () => {
 });
 
 const manageSpinner = (status) => {
-    if(status == true){
+    if (status == true) {
         document.getElementById('spinner-container').classList.remove('hidden');
         document.getElementById('main-container').classList.add('hidden');
     }
-    else{
+    else {
         document.getElementById('spinner-container').classList.add('hidden');
         document.getElementById('main-container').classList.remove('hidden');
     }
@@ -127,9 +132,91 @@ document.getElementById('input-search').addEventListener('input', () => {
     fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
         .then(res => res.json())
         .then(data => {
-            displayIssues(data.data); 
+            displayIssues(data.data);
         })
 });
 
+const loadWordDetail = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res = await fetch(url);
+    const details = await res.json();
+    displayModalDetails(details.data);
+}
+const displayModalDetails = (modalData) => {
+    console.log(modalData);
+    const detailsBox = document.getElementById('details-container');
+    const date = new Date(modalData.createdAt);
+    const formattedDate = date.toLocaleDateString();
+    detailsBox.innerHTML = `
+    <div id="details-container " class="bg-white rounded-md  p-4 space-y-6">
+        <h2 class="text-2xl text-[#1F2937] font-bold">${modalData.title}</h2>
+        <div>
+            <p class="flex items-center gap-2 text-[12px] text-[#64748B]"><button class = "btn rounded-full text-white" id="status">${modalData.status}</button><i
+                    class="fa-solid fa-circle text-[6px]"></i>${modalData.assignee ? modalData.assignee : "No Assignee"}<i
+                    class="fa-solid fa-circle text-[6px]"></i>${formattedDate}</p>
+        </div>
 
+        <div class="label-container flex items-center gap-1">
+
+        </div>
+
+        <p class="text-[#64748B] text-[1rem]">${modalData.description}</p>
+
+        <div class="grid grid-cols-2 bg-[#F8FAFC] p-4">
+            <div>
+                <p class="text-[#64748B] text-[1rem]">Assignee: </p>
+                <h2 class="font-semibold">${modalData.assignee ? modalData.assignee : "No Assignee"}</h2>
+            </div>
+            <div>
+                <p class="text-[#64748B] text-[1rem]">Priority:</p>
+                <button class="priority-container">
+                    <h2 class="text-[12px] font-medium">${modalData.priority.toUpperCase()}</h2>
+                </button>
+            </div>
+        </div>
+
+    </div>
+    `;
+
+    const labelContainer = document.getElementById('details-container').querySelector('.label-container');
+    const priorityContainer = document.getElementById('details-container').querySelector('.priority-container');
+    const priorityText = priorityContainer.querySelector('h2');
+    const status = document.getElementById('status');
+
+    if(modalData.status.toLowerCase() == "open"){
+        status.classList.add('bg-green-500');
+    }
+    else{
+        status.classList.add('bg-red-500');
+    }
+
+    if (modalData.priority.toLowerCase() === 'high') {
+            priorityContainer.classList.add('bg-[#FEECEC]', 'py-[6px]', 'px-[25px]', 'rounded-full');
+            priorityText.classList.add('text-[#EF4444]');
+        } else if (modalData.priority.toLowerCase() === 'medium') {
+            priorityContainer.classList.add('bg-[#FFF6D1]', 'py-[6px]', 'px-[25px]', 'rounded-full');
+            priorityText.classList.add('text-[#F59E0B]');
+        } else {
+            priorityContainer.classList.add('bg-[#EEEFF2]', 'py-[6px]', 'px-[25px]', 'rounded-full');
+            priorityText.classList.add('text-[#9CA3AF]');
+        }
+
+    modalData.labels.forEach(label => {
+        const labelDiv = document.createElement('div');
+        labelDiv.classList.add('py-[6px]', 'px-[8px]', 'rounded-full', 'font-medium', 'text-[12px]');
+        if (label.toLowerCase() === 'bug') {
+            labelDiv.classList.add('bg-[#FEECEC]', 'text-[#EF4444]');
+            labelDiv.innerHTML = `<i class="fa-solid fa-bug"></i> <span>${label}</span>`;
+        } else if (label.toLowerCase() === 'help wanted') {
+            labelDiv.classList.add('bg-[#fff8db]', 'text-[#D97706]');
+            labelDiv.innerHTML = `<i class="fa-solid fa-life-ring"></i> <span>${label}</span>`;
+        } else {
+            labelDiv.classList.add('bg-[#EEEFF2]', 'text-[#64748B]');
+            labelDiv.innerHTML = `<span>${label}</span>`;
+        }
+        labelContainer.appendChild(labelDiv);
+    });
+
+    document.getElementById('my_modal_5').showModal();
+}
 allIssues();
